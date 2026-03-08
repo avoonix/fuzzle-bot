38e8c07c259148d302bafc35e173c1fb64a548a5
Avoo <git@avoonix.com>
2026-03-08 08:09:03 +0100

    group recommendations by set

--- admin-ui/src/components/BanRecommendations.vue
+++ admin-ui/src/components/BanRecommendations.vue
@@ -2,6 +2,7 @@
 import { useFetch } from '@vueuse/core';
 import { computed, ref, watch } from 'vue';
 import BanStickersGroup from './BanStickersGroup.vue';
+import BanSetStickersDialog from './BanSetStickersDialog.vue';
 
 interface BanRecommendation {
   matches: StickerSimPub[],
@@ -20,6 +21,18 @@ interface StickerSimPub extends StickerPub {
 const { data, error, execute: refetch } = useFetch(`/api/recommend-stickers-for-ban`, { refetch: true, updateDataOnError: true }).json<BanRecommendation[]>()
 
 watch(error, () => console.log(error))
+
+const setMode = ref(false);
+
+const setData = computed(() => {
+  const sets: Record<string, number> = {};
+  for (const entry of data.value??[]) {
+    for (const sticker of entry.matches) {
+      sets[sticker.set_id] = (sets[sticker.set_id] ?? 0) + 1;
+    }
+  }
+  return Object.entries(sets).map(([setId, count]) => ({setId, count})).toSorted((a,b) => b.count - a.count);
+})
 
 // const maxSimilarity = ref(0.9)
 
@@ -47,6 +60,14 @@ watch(error, () => console.log(error))
 //   }
 // }
 
+const unbanSticker = async (stickerId: string) => {
+    const { data, error } = await useFetch(`/api/stickers/${stickerId}/unban`).post()
+    console.log(data, error)
+    if (error.value) {
+      alert(error.value)
+    }
+}
+
 </script>
 
 <template>
@@ -60,6 +81,20 @@ watch(error, () => console.log(error))
         step="0.005"
       ></v-slider> -->
     {{ error }}
+    
+    <v-switch v-model="setMode" label="Set Mode" />
+    
+      <div v-if="data && setMode">
+
+      <v-card v-for="set of setData" class="mb-8 mx-2">
+            <!-- <v-btn variant="plain" width="auto" height="auto" :to="{ name: 'banSetView', params: { setId: set.setid } }"> -->
+                <img :src="`/thumbnails/sticker-set/${set.setId}/image.png`" loading="lazy" width="128" height="128" />
+                {{ set }}
+              <!-- </v-btn> -->
+        <ban-set-stickers-dialog :set-id="set.setId" />
+      </v-card>
+
+      </div>
 
     <!-- <v-btn @click="toggleBan" v-if="isBanned" color="error">
       Unban
@@ -68,12 +103,16 @@ watch(error, () => console.log(error))
       Ban
     </v-btn> -->
 
-    <div v-if="data">
+    <div v-if="data && !setMode">
       <v-card v-for="rec of data" class="mb-8 mx-2">
         <v-card-text>
           banned:
           <img :src="`/api/banned-sticker/${rec.banned_sticker.id}/thumbnail.png`" loading="lazy" width="128"
             height="128" />
+
+          <v-btn @click="unbanSticker(rec.banned_sticker.id)">
+            unban sticker
+          </v-btn>
           recommended:
 
                     <ban-stickers-group :refetch="() => console.log('refetch not implemented')" :stickers="rec.matches" />
